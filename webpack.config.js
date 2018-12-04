ddc4c799b6f1b41b080651f870a9c6b49cf2095d
swyx <swyx@dontemail.me@>
2018-12-05 00:21:59 +0800

    drop alias

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,5 @@ module.exports = {
   plugins: [new webpack.DefinePlugin({ 'global.GENTLY': false })],
   node: {
     __dirname: true
-  },
-  alias: {
-    inherits: 'inherits/inherits_browser.js',
-    superagent: 'superagent/lib/client',
-    emitter: 'component-emitter'
   }
 };
